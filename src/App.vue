b5e890234683c378d3d4954db33171d4b7586d8e
zhangpanpan <zjrzpp0414@163.com>
2019-10-20 10:18:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="(user) in users" :key="user.id">
+        {{user.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/api/users').then(response => {
+      this.users = response.data;
+    })
   }
+ 
 }
 </script>
 
